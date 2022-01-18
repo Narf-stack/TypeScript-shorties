73f65860a875acb9d7ef37dfac9545e172754836
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-01-18 12:12:09 +0100

    features/annotions - update file functions

--- features/annotations/functions.ts
+++ features/annotations/functions.ts
@@ -1,44 +1,54 @@
-const add = (a: number, b: number) => {
+const add = (a: number, b: number): number => {
     return a + b;
-  };
+};
   
-  const subtract = (a: number, b: number): number => {
+/// arrow function
+const subtract = (a: number, b: number): number => {
     return a - b;
-  };
-  
-  function divide(a: number, b: number): number {
+};
+
+/// name function
+function divide(a: number, b: number): number {
     return a / b;
-  }
-  
-  const multiply = function(a: number, b: number): number {
+}
+
+/// anonymous function assign to a variable 
+const multiply = function(a: number, b: number): number {
     return a * b;
-  };
-  
-  const logger = (message: string): void => {
+};
+
+
+// Void & Never
+const logger = (message: string): void => {
     console.log(message);
-  };
-  
-  const throwError = (message: string): void => {
+};
+
+// const throwError = (message: string): never  => {
+//     throw new Error(message);
+// }; 
+const throwError = (message: string): void => {
     if (!message) {
-      throw new Error(message);
+        throw new Error(message);
     }
-  };
-  
-  const todaysWeather = {
+};
+
+
+const todaysWeather = {
     date: new Date(),
     weather: 'sunny'
-  };
-  
-  const logWeather = ({
+};
+
+//destructuring
+const logWeather = ({
     date,
     weather
-  }: {
+}: {
     date: Date;
     weather: string;
-  }): void => {
+}): void => {
     console.log(date);
     console.log(weather);
-  };
-  
-  logWeather(todaysWeather);
+};
+
+logWeather(todaysWeather);
   
