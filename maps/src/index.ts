6a627a75cd76786c0bc585c5196d1d9a2d915490
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-01-19 10:41:14 +0100

    Maps - update Google map display

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,2 +1,10 @@
+/// <reference types="@types/google.maps" />
+
 import { User } from './user'
 import { Company } from './Company'
+
+
+const map = new google.maps.Map(document.getElementById('map'),{
+    zoom: 1,
+    center: {lat: 0 , lng: 0},
+})
