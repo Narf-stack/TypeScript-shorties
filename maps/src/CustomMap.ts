4205cdd6be9902e08e09bd2579d1b92e61da00ef
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-01-19 11:04:51 +0100

    Maps - Class CustomMap definition

---
+++ maps/src/CustomMap.ts
@@ -0,0 +1,12 @@
+
+export class CustomMap {
+
+    private googleMap: google.maps.Map;
+
+    constructor(id:string){
+        this.googleMap = new google.maps.Map(document.getElementById(id),{
+            zoom: 1,
+            center: {lat: 0 , lng: 0},
+        })
+    }
+}
--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,10 +1,8 @@
 /// <reference types="@types/google.maps" />
 
 import { User } from './user'
+import { CustomMap } from './CustomMap'
 import { Company } from './Company'
 
 
-const map = new google.maps.Map(document.getElementById('map'),{
-    zoom: 1,
-    center: {lat: 0 , lng: 0},
-})
+new CustomMap('map');
