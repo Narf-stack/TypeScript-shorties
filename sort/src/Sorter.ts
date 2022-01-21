0411906cd3dbd123e7c986043d72b29588bde5fe
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-01-21 15:57:53 +0100

    Sort - Implementation interface Sortable

--- sort/src/Sorter.ts
+++ sort/src/Sorter.ts
@@ -1,7 +1,11 @@
-import { NumbersCollection } from "./NumbersCollection";
 
+interface Sortable {
+    length: number;
+    compare(leftIndex: number,rightIndex: number): boolean;
+    swap(leftIndex: number,rightIndex: number): void;
+}
 export class Sorter {
-    constructor(public collection:NumbersCollection){
+    constructor(public collection:Sortable){
     }
 
     sort():void{
@@ -10,6 +14,7 @@ export class Sorter {
             for (let j=0;j<length-i-1;j++) {
                 if (this.collection.compare(j,j+1)){
                     this.collection.swap(j,j+1)
+                }
             }
         }
     }
