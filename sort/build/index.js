69e7063f16c8c21ebff085bb974b12d2a6a8c8e6
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-01-21 15:21:55 +0100

    Sort - Implementation class Sorter

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,26 +1,5 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection instanceof Array) { // type guard
-                    if (this.collection[j] > this.collection[i + 1]) {
-                        var leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-                if (this.collection === 'string') { // type guard
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
+// import {Sorter} from 'sorter'
 var sorter = new Sorter([10, 3, -5, 0]);
 sorter.sort();
 console.log(sorter.collection);
