9e7473c888bb470cd2365e26ec31fbf8f71082d9
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-01-24 12:31:14 +0100

    Sort - Implementation Linkedlist

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
 const numberCollection = new NumbersCollection([10000,3,-5,0])
 const sorter = new Sorter(numberCollection);
@@ -12,3 +13,14 @@ const characCollection = new CharactersCollection('Xaayb')
 const stringsorter = new Sorter(characCollection);
 stringsorter.sort();
 console.log(characCollection.data)
+
+
+const linkedList = new LinkedList();
+linkedList.add(500)
+linkedList.add(-10)
+linkedList.add(-3)
+linkedList.add(4)
+
+const linksort = new Sorter(linkedList)
+linksort.sort()
+linkedList.print()
