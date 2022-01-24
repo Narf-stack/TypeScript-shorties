01336784aad4f97a667e6ba4b1daf9742fadeade
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-01-24 17:27:02 +0100

    Stats - Read & Parse CSV

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,1 +1,13 @@
 import fs from 'fs';
+
+
+// read CSV Node "fs" library
+const matches = fs.readFileSync('football.csv',{
+    encoding: 'utf-8'
+}).split('\n')
+.map((row:string):string[]=>{
+    return row.split(',');
+})
+
+
+console.log(matches)
