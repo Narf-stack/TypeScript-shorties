cb449adbf0ec38f5682ba06d8aff52c750d34965
Frantz Crozilhac <fran.stacksta@gmail.com>
2022-02-02 17:22:08 +0100

    Stats - connect everything

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,8 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var MatchReader_1 = require("./MatchReader");
-var MatchResult_1 = require("./MatchResult");
+// import { MatchResult } from './MatchResult';
 var CsvFileReader_1 = require("./CsvFileReader");
+var ConsoleReport_1 = require("./reportTargets/ConsoleReport");
+var WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
+var Summary_1 = require("./Summary");
 // read CSV Node "fs" library
 // const reader = new MatchReader('football.csv')
 // reader.read()
@@ -12,14 +15,14 @@ var csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
 //something satisfying the DataReader interface 
 var matchReader = new MatchReader_1.MatchReader(csvFileReader);
 matchReader.load();
-var manUnitedWins = 0;
-for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
-    var match = _a[_i];
-    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
-        manUnitedWins++;
-    }
-    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
-        manUnitedWins++;
-    }
-}
-console.log(manUnitedWins);
+// let manUnitedWins = 0 ;
+// for(let match of matchReader.matches){
+//     if( match[1]=== 'Man United' && match[5] === MatchResult.HomeWin){
+//         manUnitedWins++
+//     }else if( match[2]=== 'Man United' && match[5] === MatchResult.AwayWin){
+//         manUnitedWins++
+//     }
+// }
+// console.log(manUnitedWins)
+var summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new ConsoleReport_1.ConsoleReport());
+summary.buildAndPrintReport(matchReader.matches);
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,7 +1,9 @@
 import {MatchReader} from './MatchReader';
-import { MatchResult } from './MatchResult';
+// import { MatchResult } from './MatchResult';
 import { CsvFileReader } from './CsvFileReader'
-
+import { ConsoleReport } from './reportTargets/ConsoleReport';
+import { WinsAnalysis } from './analyzers/WinsAnalysis';
+import { Summary } from './Summary';
 
 
 // read CSV Node "fs" library
@@ -30,3 +32,10 @@ matchReader.load()
 // }
 
 // console.log(manUnitedWins)
+
+const summary = new Summary(
+    new WinsAnalysis('Man United'),
+    new ConsoleReport()
+)
+
+summary.buildAndPrintReport(matchReader.matches)
